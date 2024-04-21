96f7c80f19159d7f6391039d56759edfc640a056
mkurzyna <kurzynama@office.umcs.pl>
2024-04-21 17:12:45 +0200

    Lab6

--- second_project/src/Main.java
+++ second_project/src/Main.java
@@ -1,11 +1,19 @@
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
         CustomList<String> newList = new CustomList<>();
         newList.addLast("Hello");
-        System.out.println(newList.getLast());
+        //System.out.println(newList.getLast());
         newList.addLast("World");
-        System.out.println(newList.getLast());
-
+        //System.out.println(newList.getLast());
+        Iterator<String> iter = newList.iterator();
+        String text = iter.next();
+        System.out.println(text);
+        while (iter.hasNext()){
+            text = iter.next();
+            System.out.println(text);
+        }
 
 
     }
