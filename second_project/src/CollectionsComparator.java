6acebe4059dcf97f36864681c968f715e0528abc
mkurzyna <kurzynama@office.umcs.pl>
2024-04-21 17:12:28 +0200

    Lab6

---
+++ second_project/src/CollectionsComparator.java
@@ -0,0 +1,9 @@
+import java.util.Collection;
+import java.util.Comparator;
+
+public class CollectionsComparator implements Comparator<Collection<?>> {
+    @Override
+    public int compare(Collection<?> c1, Collection<?> c2) {
+        return Integer.compare(c1.size(), c2.size());
+    }
+}
